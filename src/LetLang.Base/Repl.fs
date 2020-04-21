3202bb2894fefb005e4e4ec583d0c2d250047574
David Gu <macdavid313@gmail.com>
2020-04-21 16:55:44 +1000

    skip empty input

--- src/LetLang.Base/Repl.fs
+++ src/LetLang.Base/Repl.fs
@@ -9,8 +9,11 @@ module Repl =
     [<EntryPoint>]
     let rec main argv =
         let input = ReadLine.Read("λ> ")
-        try
-            let value = run input
-            Console.WriteLine(value.ToString())
-        with e -> Console.WriteLine(e.ToString())
-        main argv
+        if input.Length <> 0 then
+            try
+                let value = run input
+                Console.WriteLine(value.ToString())
+            with e -> Console.WriteLine(e.ToString())
+            main argv
+        else
+            main argv
